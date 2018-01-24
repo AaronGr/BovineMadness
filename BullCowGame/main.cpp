280ee60bfb9e8f561096e63f79b4ab9f3be621f3
My_Desktop\Agrav <agrav12825@gmail.com>
2018-01-24 16:51:56 -0600

    Print Title Screen and Ask for a Word

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <string>
+#include "main.h"
 
 int main() {
 
-	std::cout << "Welcome to Bulls & Cows" << std::endl;
+	constexpr int WORD_LENGTH = 5;
+
+	Print_Starting_Text(WORD_LENGTH);
+
+	Get_Players_Guess(WORD_LENGTH);
+
 	return 0;
+}
+
+void Get_Players_Guess(const int &WORD_LENGTH)
+{
+	std::string guess = "";
+	std::cout << "Type a " << WORD_LENGTH;
+	std::cout << " letter word: ";
+	std::getline(std::cin, guess);
+	std::cout << "Your word was " << guess << std::endl;
+	std::cout << std::endl;
+}
+
+void Print_Starting_Text(const int &WORD_LENGTH)
+{
+	std::cout << "Welcome to Bulls & Cows" << std::endl;
+	std::cout << "Try to guess the " << WORD_LENGTH;
+	std::cout << " letter word." << std::endl;
+	std::cout << std::endl;
 }
---
+++ BullCowGame/main.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void Print_Starting_Text(const int &WORD_LENGTH);
+
+void Get_Players_Guess(const int &WORD_LENGTH);
